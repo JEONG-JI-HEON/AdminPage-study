e9de9390861ff7e7757852e5e3e8ecda5946fa60
JEONG-JI-HEON <wlgjs0804@naver.com>
2024-07-05 17:07:08 +0900

    메인페이지 디자인 수정

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,20 +1,17 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./MainPage.css";
+
+import { Button } from "antd";
 
 const MainPage = () => {
   return (
     <div className="w-full h-full flex flex-row justify-center items-center gap-4">
       <Link to="/login">
-        <button className="border border-gray-400 w-52 py-9 px-4 rounded-t-lg text-2xl font-bold border-b-4 border-b-cyan-600">
-          로그인
-        </button>
+        <Button type="primary">로그인</Button>
       </Link>
       <Link to="/admin">
-        <button className="border border-gray-400 w-52 py-9 px-4 rounded-t-lg text-2xl font-bold border-b-4 border-b-pink-600">
-          어드민페이지
-        </button>
+        <Button type="primary">어드민페이지</Button>
       </Link>
     </div>
   );
